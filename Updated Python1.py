31dc702496608de1978102bddb7efd248bdd1412
Stephen Asiedu <stephen.asiedu17@gmail.com>
2024-06-11 15:41:48 -0400

    Updated Python1.py

---
+++ Updated Python1.py
@@ -0,0 +1,36 @@
+#  Code Correction
+weather = "sunny"
+
+if weather == "sunny": 
+ print("Wear sunglasses!")
+else:
+ print("Take an umbrella!")
+    
+ 
+ 
+ #   Arithmetic Operations in Daily Life
+print("How are you feeling today?")
+
+mood = input()
+if mood == "happy":
+ print("That's great to hear")
+else:
+ print("I hope your day gets better")
+    
+    
+#3 Identifying the data type of each variable 
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+print(type(variable_a))
+print(type(variable_b))
+print(type(variable_c))
+print(type(variable_d))
+    
+#4 Arithmetic Operations in Daily Life    
+bread = 2.75
+peanut_butter = 4.50
+jelly = 3.23
+total_cost = bread + peanut_butter + jelly
+print(total_cost)
