573d66e9151d0fa1d06cf952c5b7ca035eb2a714
Stephen Asiedu <stephen.asiedu17@gmail.com>
2024-06-11 09:22:14 -0400

    My first try with python

---
+++ Homework1.py
@@ -0,0 +1,29 @@
+weather = "sunny"
+    if weather = "sunny"
+    print("Wear sunglasses!")
+    else:
+    print("Take an umbrella!")
+    
+    
+    
+print("How are you feeling today?")
+    mood = input()
+    if mood == "happy":
+    print("That's great to hear")
+    else:
+    print("I hope your day gets better")
+    
+    
+    
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+
+    print(type(variable_a))
+    print(type(variable_b))
+    print(type(variable_c))
+    print(type(variable_d))
+    
+    
